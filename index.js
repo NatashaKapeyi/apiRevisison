257ae3a84613624fc5a7f892ffb5e52f74f24a43
NatashaKapeyi <natashazikhona7@gmail.com>
2024-02-07 11:10:48 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Revision');
+const {addition} = require('./addition')
+const{subtraction}= require('./subtraction')
+
+
+
+console.log(addition(3, 6));
+console.log(subtraction(7, 3));
+console.log(subtraction(3, 7));
